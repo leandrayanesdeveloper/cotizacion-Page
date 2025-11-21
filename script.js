93f0fb5843f39271f402828791374520f92b60c5
Leandra Yanes <leandrayaneshurtado@gmail.com>
2025-11-21 10:12:40 -0400

    Correct variable names and calculate purchase result
    Fixes variable names and adds result calculation for cryptocurrency purchase.

--- script.js
+++ script.js
@@ -13,27 +13,35 @@ const cryptoSelected = [...crypto.children].find(option => option.selected).valu
 const amountValue = amount.value;
 console.log (coinSelected, cryptoSelected, amountValue)
 try {
+    const coiniNFO.innerHTML = `
+    <div class="loader"></div>
+    `;
     const response = await(await fetch(`https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${cryptoSelected}&tsyms=${coinSelected}`)).json();
     const price= response.DISPLAY[cryptoSelected][coinSelected].PRICE;
 const priceHigh= response.DISPLAY[cryptoSelected][coinSelected].HIGH24HOUR;
 const priceLow= response.DISPLAY[cryptoSelected][coinSelected].LOW24HOUR;
 const variation= response.DISPLAY [cryptoSelected][coinSelected].CHANGEPCT24HOUR;
 console.log (coinInfo)
-
+    
+// 3. Funcion de cotizacion 
+if (amounValue ≠ '') {
+    const result= Number(amountValue) / response.RAW[cryptoSelect][coinSelected].PRICE;
     coinInfo.innerHTML = `
          <p class="info"> El precio es: <span class="price">${price}</span></p>
                 <p class="info"> El precio más alto es: <span class="price">${priceHigh}</span></p>
                 <p class="info"> El precio más bajo es: <span class="price">${priceLow}</span></p>
                 <p class="info"> Variación 24 horas: <span class="price">${variation}%</span></p>
-                <p class="info"> Puedes comprar: <span class="price"> </span></p>
+                <p class="info"> Puedes comprar: <span class="price">${result.toFixed(4)} ${cryptoSelected} </span></p>
                 `;
+} else {
+    coinInfo.innerHTML = `
+         <p class="info"> El precio es: <span class="price">${price}</span></p>
+                <p class="info"> El precio más alto es: <span class="price">${priceHigh}</span></p>
+                <p class="info"> El precio más bajo es: <span class="price">${priceLow}</span></p>
+                <p class="info"> Variación 24 horas: <span class="price">${variation}%</span></p>
+                `;
+    {
+
  } catch (error){
  }
 });
-
-
-
-// 3. Funcion de cotizacion 
-
-
-// 4. Actualizar HTML con la cotizacion 
